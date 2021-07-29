451b45b70281e5e619a87ab23e0bcddf62813c1e
Dmitry Kolyago <kaliaha@icloud.com>
2021-07-29 19:00:58 +0300

    Changes on Tamkovich code review

--- src/classes.py
+++ src/classes.py
@@ -6,46 +6,59 @@ class SQLiteDataBase:
         self.db = sa.create_engine(f'sqlite:///{database_file}')
 
     def get_all_columns(self, table: str) -> '[]':
-        columns = self.db.execute(f'''
-                        PRAGMA table_info('{table}')
-                        ''')
-        return [column[1] for column in columns]
+        metadata = sa.MetaData(bind=self.db)
+        current_table = sa.Table(table, metadata, autoload=True)
+        return [column.name for column in current_table.columns]
 
     def get_all_items(self, table: str) -> '[{}, {}]':
+        metadata = sa.MetaData(bind=self.db)
+        current_table = sa.Table(table, metadata, autoload=True)
         columns = self.get_all_columns(table)
-        rows = self.db.execute(f'''
-                    SELECT * from '{table}'
-                    ''')
+        query = current_table.select()
+        rows = self.db.execute(query)
         all_items = [{column: row[index] for index, column in enumerate(columns)} for row in rows]
         return all_items
 
     def get_item_by_id(self, table: str, item_id) -> []:
-        item = self.db.execute(f'''
-                    SELECT * from '{table}'
-                    WHERE id = '{item_id}'
-                    ''')
+        metadata = sa.MetaData(bind=self.db)
+        current_table = sa.Table(table, metadata, autoload=True)
+        query = current_table.select().where(current_table.columns.id == item_id)
+        item = self.db.execute(query)
         return [elem for elem in item][0]
 
-    def add_item(self, table: str, **attributes: object) -> object:
+    def add_item(self, table: str, **attributes):
         columns = self.get_all_columns(table)
         if len(attributes) > len(columns) or not set(attributes.keys()).issubset(columns):
             raise ValueError('Wrong set of attributes for current table!')
 
-        self.db.execute(f'''
-            INSERT INTO '{table}' {tuple(attributes.keys())}
-            VALUES {tuple(attributes.values())}
-            ''')
+        metadata = sa.MetaData(bind=self.db)
+        current_table = sa.Table(table, metadata, autoload=True)
+        query = current_table.insert().values(attributes)
+        self.db.execute(query)
 
     def update_item_by_id(self, table: str, item_id, **attributes):
         columns = self.get_all_columns(table)
         if len(attributes) > len(columns) or not set(attributes.keys()).issubset(columns):
             raise ValueError('Wrong set of attributes for current table!')
 
-        self.db.execute(f'''
-            UPDATE '{table}'
-            SET {tuple(attributes.keys())} = {tuple(attributes.values())}
-            WHERE id = '{item_id}'
-            ''')
+        metadata = sa.MetaData(bind=self.db)
+        current_table = sa.Table(table, metadata, autoload=True)
+        query = current_table.update().values(attributes)
+
+        query = current_table.update().where(current_table.columns.id == item_id)
+
+        self.db.execute(query)
+
+
+        # columns = self.get_all_columns(table)
+        # if len(attributes) > len(columns) or not set(attributes.keys()).issubset(columns):
+        #     raise ValueError('Wrong set of attributes for current table!')
+        #
+        # self.db.execute(f'''
+        #     UPDATE '{table}'
+        #     SET {tuple(attributes.keys())} = {tuple(attributes.values())}
+        #     WHERE id = '{item_id}'
+        #     ''')
 
     def delete_item_by_id(self, table: str, item_id):
         self.db.execute(f'''
