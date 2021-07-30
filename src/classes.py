bf1197a633089fac1493e2b049f7cea47e766e87
Dmitry Kolyago <kaliaha@icloud.com>
2021-07-30 08:15:54 +0300

    Changes on Tamkovich code review

--- src/classes.py
+++ src/classes.py
@@ -5,12 +5,19 @@ class SQLiteDataBase:
     def __init__(self, database_file: str):
         self.db = sa.create_engine(f'sqlite:///{database_file}')
 
-    def get_all_columns(self, table: str) -> '[]':
+    def get_all_columns(self, table: str) -> list:
         metadata = sa.MetaData(bind=self.db)
         current_table = sa.Table(table, metadata, autoload=True)
         return [column.name for column in current_table.columns]
 
-    def get_all_items(self, table: str) -> '[{}, {}]':
+    def get_all_items(self, table: str) -> list:
+        """Returns all items from table
+
+       :param table: table name from database
+       :returns: [{column1: value1, column2: value2...},
+                  {column1: value1, column2: value2...}, ... ]
+       """
+
         metadata = sa.MetaData(bind=self.db)
         current_table = sa.Table(table, metadata, autoload=True)
         columns = self.get_all_columns(table)
@@ -19,7 +26,7 @@ class SQLiteDataBase:
         all_items = [{column: row[index] for index, column in enumerate(columns)} for row in rows]
         return all_items
 
-    def get_item_by_id(self, table: str, item_id) -> []:
+    def get_item_by_id(self, table: str, item_id) -> list:
         metadata = sa.MetaData(bind=self.db)
         current_table = sa.Table(table, metadata, autoload=True)
         query = current_table.select().where(current_table.columns.id == item_id)
@@ -43,25 +50,11 @@ class SQLiteDataBase:
 
         metadata = sa.MetaData(bind=self.db)
         current_table = sa.Table(table, metadata, autoload=True)
-        query = current_table.update().values(attributes)
-
-        query = current_table.update().where(current_table.columns.id == item_id)
-
+        query = current_table.update().where(current_table.columns.id == item_id).values(attributes)
         self.db.execute(query)
 
-
-        # columns = self.get_all_columns(table)
-        # if len(attributes) > len(columns) or not set(attributes.keys()).issubset(columns):
-        #     raise ValueError('Wrong set of attributes for current table!')
-        #
-        # self.db.execute(f'''
-        #     UPDATE '{table}'
-        #     SET {tuple(attributes.keys())} = {tuple(attributes.values())}
-        #     WHERE id = '{item_id}'
-        #     ''')
-
     def delete_item_by_id(self, table: str, item_id):
-        self.db.execute(f'''
-            DELETE FROM '{table}'
-            WHERE id = '{item_id}'
-            ''')
+        metadata = sa.MetaData(bind=self.db)
+        current_table = sa.Table(table, metadata, autoload=True)
+        query = current_table.delete().where(current_table.columns.id == item_id)
+        self.db.execute(query)
